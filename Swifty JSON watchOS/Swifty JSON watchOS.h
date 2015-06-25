e090afde9d30db56aa7beb88dda53058e9f59c09
Daniel Brooks <daniel_brooks@dabsquared.com>
2015-06-24 22:25:55 -0700

    Adding a watchOS Framework

---
+++ Swifty JSON watchOS/Swifty JSON watchOS.h
@@ -0,0 +1,19 @@
+//
+//  Swifty JSON watchOS.h
+//  Swifty JSON watchOS
+//
+//  Created by Daniel Brooks on 6/24/15.
+//
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Swifty JSON watchOS.
+FOUNDATION_EXPORT double Swifty_JSON_watchOSVersionNumber;
+
+//! Project version string for Swifty JSON watchOS.
+FOUNDATION_EXPORT const unsigned char Swifty_JSON_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swifty_JSON_watchOS/PublicHeader.h>
+
+

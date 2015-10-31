37ffd5d7241f48684e924e8b18695b193e7ad6fd
Kyle Fang <zhigang1992@gmail.com>
2015-10-31 08:55:22 +0800

    Add tvOS

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
+
 xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON iOS" -destination "platform=iOS Simulator,name=iPhone 6" test
+
 xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON OSX" test
 
-# Not running tvOS since Travis CI doesn't yet support it.
-# xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON tvOS" -destination "platform=tvOS Simulator,name=Apple TV 1080p" test
+xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON tvOS" -destination "platform=tvOS Simulator,name=Apple TV 1080p" test

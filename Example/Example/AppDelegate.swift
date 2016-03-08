75a5b96aaa27f0e13f5e653d02ac2516cf253fd8
Robin <1933549736@qq.com>
2016-03-08 17:25:52 +0800

    delete test

--- Example/Example/AppDelegate.swift
+++ Example/Example/AppDelegate.swift
@@ -40,26 +40,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             viewController.json = json
         } else {
             viewController.json = JSON.null
-        }
-        
-        testConvertible()
+        } 
         
         return true
-    }
-    
-    func testConvertible() {
-        var json: JSON = ["columns" : ["created_at" : "DESC", "id" : "DESC"]]
-        
-        let jsonDic = json["columns"].dictionary
-        
-        var result: [String : String] = [:]
-        if let jsonDic = jsonDic {
-            for (key, value) in jsonDic {
-                result[key] = value.stringValue
-            }
-        }
-        
-        print("\(result)") //["created_at": "DESC", "id": "DESC"]
-    }
+    } 
 }
 

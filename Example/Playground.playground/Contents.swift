d2367f7df4608649c718b41dc8eab50db98327ee
Jeff Kang <sugar1230@gmail.com>
2016-09-01 23:01:15 +0900

    Basic playground set.

--- Example/Playground.playground/Contents.swift
+++ Example/Playground.playground/Contents.swift
@@ -1,3 +1,14 @@
 //: Playground - noun: a place where people can play
 
 import SwiftyJSON
+
+var testData: NSData?
+
+if let file = NSBundle.mainBundle().pathForResource("SwiftyJSONTests", ofType: "json") {
+    testData = NSData(contentsOfFile: file)
+} else {
+    print("Fail")
+}
+
+let json = JSON(data: testData!)
+print(json[0]["text"])

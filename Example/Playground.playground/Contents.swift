cdca18303a3613e7996b0978654e485baf3329a5
Jeff Kang <sandfox2010@gmail.com>
2016-10-16 16:57:36 +0900

    Basic playground docs.

--- Example/Playground.playground/Contents.swift
+++ Example/Playground.playground/Contents.swift
@@ -12,3 +12,20 @@ if let file = Bundle.main.path(forResource: "SwiftyJSONTests", ofType: "json") {
 
 let json = JSON(data: testData!)
 print(json[0]["text"])
+
+
+/*:
+ ## Usage
+ 
+ #### Initialization
+ */
+
+
+import SwiftyJSON
+
+let json = JSON(data: dataFromNetworking)
+let json = JSON(jsonObject)
+
+if let dataFromString = jsonString.dataUsingEncoding(NSUTF8StringEncoding, allowLossyConversion: false) {
+    let json = JSON(data: dataFromString)
+}

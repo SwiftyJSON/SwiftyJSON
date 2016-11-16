e64f17ffdbc3a11bafbb200b6d5e07933d7c603e
Zhigang Fang <zhigang1992@gmail.com>
2016-11-16 23:07:25 +0800

    Add performance test

--- Tests/SwiftyJSONTests/SequenceTypeTests.swift
+++ Tests/SwiftyJSONTests/SequenceTypeTests.swift
@@ -46,7 +46,7 @@ class SequenceTypeTests: XCTestCase {
             XCTFail("Can't find the test JSON file")
         }
     }
-    
+
     func testArrayAllNumber() {
         var json:JSON = [1,2.0,3.3,123456789,987654321.123456789]
         XCTAssertEqual(json.count, 5)
@@ -62,11 +62,11 @@ class SequenceTypeTests: XCTestCase {
         XCTAssertEqual(index, 5)
         XCTAssertEqual(array, [1,2.0,3.3,123456789,987654321.123456789])
     }
-    
+
     func testArrayAllBool() {
         var json:JSON = JSON([true, false, false, true, true])
         XCTAssertEqual(json.count, 5)
-        
+
         var index = 0
         var array = [Bool]()
         for (i, sub) in json {
@@ -78,11 +78,11 @@ class SequenceTypeTests: XCTestCase {
         XCTAssertEqual(index, 5)
         XCTAssertEqual(array, [true, false, false, true, true])
     }
-    
+
     func testArrayAllString() {
         var json:JSON = JSON(rawValue: ["aoo","bpp","zoo"] as NSArray)!
         XCTAssertEqual(json.count, 3)
-        
+
         var index = 0
         var array = [String]()
         for (i, sub) in json {
@@ -94,11 +94,11 @@ class SequenceTypeTests: XCTestCase {
         XCTAssertEqual(index, 3)
         XCTAssertEqual(array, ["aoo","bpp","zoo"])
     }
-    
+
     func testArrayWithNull() {
         var json:JSON = JSON(rawValue: ["aoo","bpp", NSNull() ,"zoo"] as NSArray)!
         XCTAssertEqual(json.count, 4)
-        
+
         var index = 0
         var array = [AnyObject]()
         for (i, sub) in json {
@@ -111,11 +111,11 @@ class SequenceTypeTests: XCTestCase {
         XCTAssertEqual(array[0] as? String, "aoo")
         XCTAssertEqual(array[2] as? NSNull, NSNull())
     }
-    
+
     func testArrayAllDictionary() {
         var json:JSON = [["1":1, "2":2], ["a":"A", "b":"B"], ["null":NSNull()]]
         XCTAssertEqual(json.count, 3)
-        
+
         var index = 0
         var array = [AnyObject]()
         for (i, sub) in json {
@@ -131,11 +131,11 @@ class SequenceTypeTests: XCTestCase {
         XCTAssertEqual((array[1] as! [String : String])["b"]!, "B")
         XCTAssertEqual((array[2] as! [String : NSNull])["null"]!, NSNull())
     }
-    
+
     func testDictionaryAllNumber() {
         var json:JSON = ["double":1.11111, "int":987654321]
         XCTAssertEqual(json.count, 2)
-        
+
         var index = 0
         var dictionary = [String:NSNumber]()
         for (key, sub) in json {
@@ -143,16 +143,16 @@ class SequenceTypeTests: XCTestCase {
             dictionary[key] = sub.number!
             index += 1
         }
-        
+
         XCTAssertEqual(index, 2)
         XCTAssertEqual(dictionary["double"]! as NSNumber, 1.11111)
         XCTAssertEqual(dictionary["int"]! as NSNumber, 987654321)
     }
-    
+
     func testDictionaryAllBool() {
         var json:JSON = ["t":true, "f":false, "false":false, "tr":true, "true":true]
         XCTAssertEqual(json.count, 5)
-        
+
         var index = 0
         var dictionary = [String:Bool]()
         for (key, sub) in json {
@@ -160,16 +160,16 @@ class SequenceTypeTests: XCTestCase {
             dictionary[key] = sub.bool!
             index += 1
         }
-        
+
         XCTAssertEqual(index, 5)
         XCTAssertEqual(dictionary["t"]! as Bool, true)
         XCTAssertEqual(dictionary["false"]! as Bool, false)
     }
-    
+
     func testDictionaryAllString() {
         var json:JSON = JSON(rawValue: ["a":"aoo","bb":"bpp","z":"zoo"] as NSDictionary)!
         XCTAssertEqual(json.count, 3)
-        
+
         var index = 0
         var dictionary = [String:String]()
         for (key, sub) in json {
@@ -177,16 +177,16 @@ class SequenceTypeTests: XCTestCase {
             dictionary[key] = sub.string!
             index += 1
         }
-        
+
         XCTAssertEqual(index, 3)
         XCTAssertEqual(dictionary["a"]! as String, "aoo")
         XCTAssertEqual(dictionary["bb"]! as String, "bpp")
     }
-    
+
     func testDictionaryWithNull() {
         var json:JSON = JSON(rawValue: ["a":"aoo","bb":"bpp","null":NSNull(), "z":"zoo"] as NSDictionary)!
         XCTAssertEqual(json.count, 4)
-        
+
         var index = 0
         var dictionary = [String:AnyObject]()
         for (key, sub) in json {
@@ -194,18 +194,18 @@ class SequenceTypeTests: XCTestCase {
             dictionary[key] = sub.object as AnyObject?
             index += 1
         }
-        
+
         XCTAssertEqual(index, 4)
         XCTAssertEqual(dictionary["a"]! as? String, "aoo")
         XCTAssertEqual(dictionary["bb"]! as? String, "bpp")
         XCTAssertEqual(dictionary["null"]! as? NSNull, NSNull())
     }
-    
+
     func testDictionaryAllArray() {
         var json:JSON = JSON (["Number":[NSNumber(value:1),NSNumber(value:2.123456),NSNumber(value:123456789)], "String":["aa","bbb","cccc"], "Mix":[true, "766", NSNull(), 655231.9823]])
 
         XCTAssertEqual(json.count, 3)
-        
+
         var index = 0
         var dictionary = [String:AnyObject]()
         for (key, sub) in json {
@@ -213,7 +213,7 @@ class SequenceTypeTests: XCTestCase {
             dictionary[key] = sub.object as AnyObject?
             index += 1
         }
-        
+
         XCTAssertEqual(index, 3)
         XCTAssertEqual((dictionary["Number"] as! NSArray)[0] as? Int, 1)
         XCTAssertEqual((dictionary["Number"] as! NSArray)[1] as? Double, 2.123456)
@@ -222,5 +222,17 @@ class SequenceTypeTests: XCTestCase {
         XCTAssertEqual((dictionary["Mix"] as! NSArray)[1] as? String, "766")
         XCTAssertEqual((dictionary["Mix"] as! NSArray)[2] as? NSNull, NSNull())
         XCTAssertEqual((dictionary["Mix"] as! NSArray)[3] as? Double, 655231.9823)
+    }
+
+    func testDictionaryIteratingPerformance() {
+        var json: JSON = [:]
+        for i in 1...1000 {
+            json[String(i)] = "hello"
+        }
+        measure {
+            for (key, value) in json {
+                print(key, value)
+            }
+        }
     }
 }

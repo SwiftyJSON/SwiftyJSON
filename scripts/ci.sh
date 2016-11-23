ae338a61420df6fc06f7d85fe7c219d8ad243ae8
Zigii Wong <wongzigii@gmail.com>
2016-11-23 23:21:53 +0800

    Update scripts

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON iOS" -destination "platform=iOS Simulator,name=iPhone 6" test
+travis_retry xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON iOS" -destination "platform=iOS Simulator,name=iPhone 6" build-for-testing test | xcpretty
 
-xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON OSX" test
+travis_retry xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON OSX" build-for-testing test | xcpretty
 
-xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON tvOS" -destination "platform=tvOS Simulator,name=Apple TV 1080p" test
+travis_retry xcodebuild -workspace SwiftyJSON.xcworkspace -scheme "SwiftyJSON tvOS" -destination "platform=tvOS Simulator,name=Apple TV 1080p" build-for-testing test | xcpretty

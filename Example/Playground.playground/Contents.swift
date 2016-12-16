dfff1b7fa2cb40aa0510a1d8287da2b8e1dce5b9
Jeff Kang <sandfox2010@gmail.com>
2016-12-16 13:35:23 +0900

    Add Error part on playground

--- Example/Playground.playground/Contents.swift
+++ Example/Playground.playground/Contents.swift
@@ -108,3 +108,47 @@ for (index,subJson):(String, JSON) in json["array"] {
 	//Do something you want
 	print("\(index): \(subJson)")
 }
+
+/*:
+ ### Error
+
+Use a subscript to get/set a value in an Array or Dictionary
+
+If the JSON is:
+
+- an array, the app may crash with "index out-of-bounds."
+- a dictionary, it will be assigned nil without a reason.
+- not an array or a dictionary, the app may crash with an "unrecognised selector" exception.
+
+This will never happen in SwiftyJSON.
+*/
+
+let errorJson = JSON(["name", "age"])
+if let name = errorJson[999].string {
+	//Do something you want
+} else {
+	print(errorJson[999].error) // "Array[999] is out of bounds"
+}
+
+let errorJson2 = JSON(["name":"Jack", "age": 25])
+if let name = errorJson2["address"].string {
+	//Do something you want
+} else {
+	print(errorJson2["address"].error) // "Dictionary["address"] does not exist"
+}
+
+let errorJson3 = JSON(12345)
+if let age = errorJson3[0].string {
+	//Do something you want
+} else {
+	print(errorJson3[0])       // "Array[0] failure, It is not an array"
+	print(errorJson3[0].error) // "Array[0] failure, It is not an array"
+}
+
+if let name = json["name"].string {
+	//Do something you want
+} else {
+	print(json["name"])       // "Dictionary[\"name"] failure, It is not an dictionary"
+	print(json["name"].error) // "Dictionary[\"name"] failure, It is not an dictionary"
+}
+

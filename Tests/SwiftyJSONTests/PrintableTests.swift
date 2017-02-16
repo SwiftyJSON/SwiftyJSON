57f5ff6630aeedac62101b219f732d6a559ee09f
Guillaume Sabran <sabranguillaume@gmail.com>
2017-02-15 16:25:08 -0800

    Handle dictionaries that have JSON values

--- Tests/SwiftyJSONTests/PrintableTests.swift
+++ Tests/SwiftyJSONTests/PrintableTests.swift
@@ -120,5 +120,28 @@ class PrintableTests: XCTestCase {
         XCTAssertTrue(description.range(of: "\"1\":2", options: NSString.CompareOptions.caseInsensitive) != nil)
         XCTAssertTrue(description.range(of: "\"2\":\"two\"", options: NSString.CompareOptions.caseInsensitive) != nil)
         XCTAssertTrue(description.range(of: "\"3\":null", options: NSString.CompareOptions.caseInsensitive) != nil)
-    }
+	}
+	
+	func testJson() {
+		let dictionary = ["1":2,"2":2,"three":3,"list":["aa","bb","dd"], "foo": []] as NSDictionary
+		let json = JSON(dictionary)
+		guard var description = json.rawString([.castNilToNSNull: true]) else {
+			XCTFail("could not represent dictionary")
+			return
+		}
+		description = description.replacingOccurrences(of: "\n", with: "")
+		description = description.replacingOccurrences(of: " ", with: "")
+
+		let dict2 = ["json": json, "baa": "boo"] as [String: Any?]
+		let json2 = JSON(dict2)
+
+		guard var description2 = json2.rawString([.castNilToNSNull: true]) else {
+			XCTFail("could not represent json")
+			return
+		}
+		description2 = description2.replacingOccurrences(of: "\n", with: "")
+		description2 = description2.replacingOccurrences(of: " ", with: "")
+		XCTAssertTrue(description2.range(of: "\"json\":{", options: NSString.CompareOptions.caseInsensitive) != nil)
+		XCTAssertTrue(description2.range(of: description, options: NSString.CompareOptions.caseInsensitive) != nil)
+	}
 }

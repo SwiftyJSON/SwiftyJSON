2bdc9d111f320baf93e644e6fced465da9bd5bac
Quan Vo <quan.vo@ibm.com>
2017-07-11 10:22:53 -0500

    Update copyright year

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 /**
- * Copyright IBM Corporation 2016
+ * Copyright IBM Corporation 2016, 2017
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

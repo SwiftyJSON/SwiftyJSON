a928c4a3f011c09d3d8159e90a1d4227094ffdd7
Moritz Vetter <mv@3yourmind.com>
2019-03-28 00:35:33 +0100

    Updated SwiftyJSON to swift-tools 5.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,14 @@
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyJSON")
+    name: "SwiftyJSON",
+    products: [
+        .library(name: "SwiftyJSON", targets: ["SwiftyJSON"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(name: "SwiftyJSON", dependencies: []),
+        .testTarget(name: "SwiftJSONTests"),
+    ]
+)

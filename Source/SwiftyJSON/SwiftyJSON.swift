228cd3e5529fc7d9aed2d1de2e462575b9690e9c
Alexander Antonov <a.b.antonov@tinkoff.ru>
2020-05-21 13:14:05 +0300

    Update collection conformance

--- Source/SwiftyJSON/SwiftyJSON.swift
+++ Source/SwiftyJSON/SwiftyJSON.swift
@@ -328,34 +328,44 @@ extension JSON: Swift.Collection {
     public typealias Index = JSONRawIndex
 
     public var startIndex: Index {
-        switch type {
-        case .array:      return .array(rawArray.startIndex)
-        case .dictionary: return .dictionary(rawDictionary.startIndex)
+        switch content {
+        case .array(let rawArray): return .array(rawArray.startIndex)
+        case .dictionary(let rawDictionary): return .dictionary(rawDictionary.startIndex)
         default:          return .null
         }
     }
 
     public var endIndex: Index {
-        switch type {
-        case .array:      return .array(rawArray.endIndex)
-        case .dictionary: return .dictionary(rawDictionary.endIndex)
+        switch content {
+        case .array(let rawArray):      return .array(rawArray.endIndex)
+        case .dictionary(let rawDictionary): return .dictionary(rawDictionary.endIndex)
         default:          return .null
         }
     }
 
     public func index(after i: Index) -> Index {
-        switch i {
-        case .array(let idx):      return .array(rawArray.index(after: idx))
-        case .dictionary(let idx): return .dictionary(rawDictionary.index(after: idx))
-        default:                   return .null
+        switch content {
+        case .array(let rawArray):
+            guard case .array(let idx) = i else { return .null }
+            return .array(rawArray.index(after: idx))
+        case .dictionary(let rawDictionary):
+            guard case .dictionary(let idx) = i else { return .null }
+            return .dictionary(rawDictionary.index(after: idx))
+        default:
+            return .null
         }
     }
 
     public subscript (position: Index) -> (String, JSON) {
-        switch position {
-        case .array(let idx):      return (String(idx), JSON(rawArray[idx]))
-        case .dictionary(let idx): return (rawDictionary[idx].key, JSON(rawDictionary[idx].value))
-        default:                   return ("", JSON.null)
+        switch content {
+        case .array(let rawArray):
+            guard case .array(let idx) = position else { return ("", JSON.null) }
+            return (String(idx), JSON(rawArray[idx]))
+        case .dictionary(let rawDictionary):
+            guard case .dictionary(let idx) = position else { return ("", JSON.null) }
+            return (rawDictionary[idx].key, JSON(rawDictionary[idx].value))
+        default:
+            return ("", JSON.null)
         }
     }
 }
